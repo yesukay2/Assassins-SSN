cdeb1620708f86c89c81f527678b76230c5760f7
Yesu K. Apraku <60203028+yesukay2@users.noreply.github.com>
2024-12-17 08:56:48 -0500

    Delete config/constants.cjs

--- config/constants.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://yesukay2:BIsNbrpZa1QBEMO2@cluster0.umiky.mongodb.net/',
-    BLOCKCYPHER_API_URL: 'https://api.blockcypher.com/v1/btc/main',
-    BTC_ADDRESS: 'bc1qsj55rn9wl8xghdaskcj8m8mrsjyax6h6w6c0xx',
-    API_TOKEN: 'e34fbb9ea1504ac68562475aa922b7c5',
-    BOT_TOKEN: '7920145612:AAFB3wwnyj4tn60vR4DhsC9p-dczLdyyTO4',
-    REQUIRED_CONFIRMATIONS: 1,
-};
