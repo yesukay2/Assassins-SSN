88b75208e030fbdaf160d7ac98c34cf9e3b908ac
Yesukay <yesukay2@gmail.com>
2024-12-17 11:03:35 -0500

    cleaning

---
+++ config/constants.cjs
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+
+
+
+module.exports = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    BLOCKCYPHER_API_URL: process.env.BLOCKCYPHER_API_URL,
+    BTC_ADDRESS: process.env.BTC_ADDRESS,
+    API_TOKEN: process.env.API_TOKEN,
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    REQUIRED_CONFIRMATIONS: process.env.REQUIRED_CONFIRMATIONS,
+};
