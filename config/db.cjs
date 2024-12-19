888b6c764d55d795b9aa8fbddfde90cff2e11f0d
Yesukay <yesukay2@gmail.com>
2024-12-19 18:29:11 -0500

    df

--- config/db.cjs
+++ config/db.cjs
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./constants.cjs');
 
 async function connectDB() {
+    console.log(`Connecting to MongoDB...`, MONGODB_URI);
     try {
         await mongoose.connect(MONGODB_URI);
         console.log('MongoDB connected');
