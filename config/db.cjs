afbbeeb752077ae707cc3a424b18987282e7b93e
Yesukay <yesukay2@gmail.com>
2024-12-19 17:55:33 -0500

    try

--- config/db.cjs
+++ config/db.cjs
@@ -3,10 +3,7 @@ const { MONGODB_URI } = require('./constants.cjs');
 
 async function connectDB() {
     try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(MONGODB_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
